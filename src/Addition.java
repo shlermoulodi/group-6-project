2d791cb2b72ef3e85f5cd85c96236a36c2c006b8
Daniel <daniel.gidlund@outlook.com>
2021-06-21 13:56:16 +0200

    addition

---
+++ src/Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 17;
+        int c = 342;
+        int d = a + b + c;
+        System.out.println("Addition result = " + d);
+    }
+}
